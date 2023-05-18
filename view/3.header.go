779ca9057d6e27d88b83bb88f8e7248848f9326d
yintianxiong <yintx_129@163.com>
2023-05-18 15:44:02 +0800

    [gin]:请求头

--- view/3.header.go
+++ view/3.header.go
@@ -1,8 +1,32 @@
+//go:build ignore
+
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	
+	router.GET("/header", func(c *gin.Context) {
+		//不区分首字母大小写
+		fmt.Println(c.GetHeader("User-Agent"))
+
+		fmt.Println(c.Request.Header)
+
+		fmt.Println(c.Request.Header.Get("User-Agent"))
+
+		c.JSON(http.StatusOK, gin.H{"msg": "sucessful"})
+	})
+
+	//响应头
+	router.GET("/header/response", func(c *gin.Context) {
+		c.Header("Response-Token", "yintx")
+		c.JSON(http.StatusOK, gin.H{"msg": "response"})
+	})
+
+	router.Run(":8081")
 }
